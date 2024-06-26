34fc1578c5fa782c68f544f128e5b35ab0818519
Ramon Mesquita <ramonmesquita113@gmail.com>
2024-06-25 21:42:22 -0300

    Extraindo nossa camada de consumer

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -14,7 +14,7 @@ public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             var key = UUID.randomUUID().toString();
             var value = key + "121212,141414,1515151515";
             var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
